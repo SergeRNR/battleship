0fd85131ef3ef74408516ddaf13ec14daaf745b2
Serge Melnikov <smelnikov@xbsoftware.com>
2015-03-02 11:30:33 +0300

    [add] cells click handler

--- public/js/controllers/battlefieldCtrl.js
+++ public/js/controllers/battlefieldCtrl.js
@@ -2,8 +2,16 @@ define(['./module'], function (controllers) {
     'use strict';
     controllers.controller('BattlefieldCtrl', ['$scope', 'BattlefieldService',
         function ($scope, BFS) {
+            var cells = [];
+
+            $scope.cells = cells;
             $scope.matrixClick = function (e) {
-                console.log(BFS.getCoords(e));
+                if (e.target.className.indexOf('fleet') !== -1) {
+                    var code = BFS.getXYCode(e),
+                        xy = BFS.getCodeXY(code, e.currentTarget);
+                    console.log(code);
+                    cells.push({top: xy.y, left: xy.x, size: xy.s});
+                }
             };
         }
     ]);
--- public/js/services/battlefieldService.js
+++ public/js/services/battlefieldService.js
@@ -14,16 +14,32 @@ define([
             el.style.height = el.style.width = size+'px';
         };
 
-        this.getCoords = function (e) {
+        this.getXYCode = function (e) {
             var props = e.target.getBoundingClientRect(),
+                cell_size = (props.width-2) / 10,
                 cursor_correction = 14,
                 x = e.x - props.left + cursor_correction,
                 y = e.y - props.top + cursor_correction;
 
-            x = Math.floor(x / ( (props.width-2) / 10));
-            y = Math.floor(y / ( (props.height-2) / 10));
+            x = Math.floor(x / cell_size); // cell index
+            x = Math.min(x, 9); // prevent fault of custom cursor
+            y = Math.floor(y / cell_size);
+            y = Math.min(y, 9);
 
             return ROWS[y] + COLS[x];
+        };
+
+        this.getCodeXY = function (code, el) {
+            var props = el.getBoundingClientRect(),
+                cell_size = (props.width-2) / 10,
+                arr = code.split(''),
+                x = arr[2] ? COLS.indexOf(arr[1]+arr[2]) : COLS.indexOf(arr[1]),
+                y = ROWS.indexOf(arr[0]);
+
+            x = props.left + x * cell_size;
+            y = props.top + y * cell_size;
+
+            return {x:x, y:y, s:cell_size};
         };
 
         this.testRandom = function () {
